8ef736c4c12e5a5edd4835a254b0e338b0251275
Kevin Eduardo <5313724+kevineduardo@users.noreply.github.com>
2024-10-17 16:29:58 +0100

    Update entrypoint.sh

--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -2,4 +2,14 @@
 php artisan filament:upgrade
 php artisan log-viewer:publish
 php artisan package:discover --ansi
-chown -R application:application /app
+rm -r -f ./public/storage
+php artisan storage:link
+chown -R application:application /app 
+find /app -type f -print0 | xargs -0 chmod 644
+find /app -type d -print0 | xargs -0 chmod 755
+chmod a+x /app/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64
+php artisan migrate --force
+php artisan optimize:clear
+php artisan optimize
+php artisan icons:cache
+php artisan config:clear
